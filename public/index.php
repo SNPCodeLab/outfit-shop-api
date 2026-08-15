811112a0cf3778639541dfe110d193f269a5c2a1
shiliaiwei <70220432+shiliaiwei@users.noreply.github.com>
2026-08-16 02:43:54 +0700

    fix(public): bind Facade application instance in public/index.php for Vercel serverless error rendering

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Facade;
 
 define('LARAVEL_START', microtime(true));
 
@@ -16,5 +17,7 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+Facade::setFacadeApplication($app);
 
 $app->handleRequest(Request::capture());
