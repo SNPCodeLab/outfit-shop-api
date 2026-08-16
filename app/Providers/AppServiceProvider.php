6bed5d3526857355d73984832fb18297ba48e9a8
shiliaiwei <70220432+shiliaiwei@users.noreply.github.com>
2026-08-16 12:11:10 +0700

    fix: bind FileBasedMaintenanceMode in AppServiceProvider to prevent driver resolution failure

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Foundation\MaintenanceMode as MaintenanceModeContract;
+use Illuminate\Foundation\FileBasedMaintenanceMode;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(MaintenanceModeContract::class, function () {
+            return new FileBasedMaintenanceMode();
+        });
     }
 
     /**
